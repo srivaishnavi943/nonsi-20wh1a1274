27870eb7bf88f4bf5a4e64c5f013367c75c23e19
srivaishnavi943 <85931786+srivaishnavi943@users.noreply.github.com>
2022-10-31 11:23:08 +0530

    Add files via upload

---
+++ nCr.c++.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int fact(int n)
+
+{
+    if (n<=1){
+       return 1;
+    }
+    else{
+    return n*fact(n-1);
+    }
+}
+int nCr(int n,int r)
+{
+    return fact(n)/(fact(n-r)*fact(r));
+}    
+int main()
+{
+    int n,r;
+    cout<<"enter n: ";
+    cin>>n;
+    
+    cout<<"enter r: ";
+    cin>>r;
+    
+    cout<<n<<"C"<<r<< "=" <<nCr(n,r);
+    
+
+}
+
+
